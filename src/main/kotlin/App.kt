19c341e4642149b066667f042656a4f045293efd
Marvin Borner <marvin-borner@live.com>
2019-04-02 20:21:59 +0200

    Added basic upload functionality

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,13 +1,12 @@
 package space.anity
 
-import io.javalin.Javalin
-import io.javalin.NotFoundResponse
-import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
+import io.javalin.*
+import io.javalin.core.util.*
+import java.io.*
+import java.nio.file.*
 
 fun main(args: Array<String>) {
-    val app = Javalin.create().start(7000)
+    val app = Javalin.create().enableStaticFiles("../resources/").start(7000)
     val fileHome = "files"
 
     app.get("/") { ctx ->
@@ -25,9 +24,18 @@ fun main(args: Array<String>) {
             }
             ctx.result(files)
         } catch (_: java.nio.file.NoSuchFileException) {
-            throw NotFoundResponse("File or directory does not exist")
+            throw NotFoundResponse("Error: File or directory does not exist.")
         }
 
         //File("test").writeText(ctx.splat(0)!!)
+    }
+
+    app.post("/upload") { ctx ->
+        ctx.uploadedFiles("files").forEach { (contentType, content, name, extension) ->
+            if (ctx.queryParam("dir") !== null)
+                FileUtil.streamToFile(content, "files/${ctx.queryParam("dir")}/$name")
+            else
+                throw BadRequestResponse("Error: Please enter a filename.")
+        }
     }
 }
