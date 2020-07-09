7023dc2a6920aa6389ac2b28bcdbaacdb4413e00
LarsVomMars <lars@kroenner.eu>
2020-07-10 00:09:39 +0200

    Make use of the renderer and fixed vscode debugger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { Application, Context } from "https://deno.land/x/abc@v1/mod.ts";
+import { renderFile } from "https://deno.land/x/dejs/mod.ts";
 import "https://deno.land/x/dotenv/load.ts";
 
 const port = parseInt(Deno.env.get("PORT") || "8080");
 const app = new Application();
 
+app.renderer = {
+  render<T>(name: string, data: T): Promise<Deno.Reader> {
+    return renderFile(name, data);
+  },
+};
+
 app.static("/", "./src/public/"); // Manage static files
-app.file("/", "./src/public/index.html"); // Render index on /
+app.get("/", async (c: Context) => await c.render("./src/public/index.html")); // Render index on /
 
 app.start({ port });
 console.log(`Server listening on http://localhost:${port}`);
