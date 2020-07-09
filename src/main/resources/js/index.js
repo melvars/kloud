1edfdf5f3a316a36108a0a853b0a2553d116d6fc
Marvin Borner <develop@marvinborner.de>
2020-07-09 22:33:43 +0200

    Rewriiiiiiiiiite!
    Okay, okay. I know, rewriting projects all the time is dumb.
    Buuut, we really don't want to work with our old and ugly code anymore.
    Furthermore we'll be using Deno now!

--- src/main/resources/js/index.js
+++
@@ -1,8 +0,0 @@
-if (document.querySelector("#toggle") !== null) {
-    document.querySelector("#toggle").addEventListener("click", () => {
-        const request = new XMLHttpRequest();
-        request.open("POST", "/user/theme");
-        request.onload = () => location.reload();
-        request.send();
-    });
-}
--- src/main/resources/js/login.js
+++
@@ -1,8 +0,0 @@
-const tryAgain = document.getElementById("tryAgain");
-const countdown = document.getElementById("counter");
-
-if (tryAgain !== null)
-    setInterval(() => {
-        if (Number(countdown.innerText) === 0) tryAgain.style.display = "none";
-        countdown.innerText = Number(countdown.innerText) - 1;
-    }, 1000);
