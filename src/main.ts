5c6e6e9962187335d80ee8f31921ee561c430365
Marvin Borner <develop@marvinborner.de>
2020-07-09 22:54:19 +0200

    HEAVY CHANGES

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { Application } from "https://deno.land/x/abc@v1/mod.ts";
 
 const app = new Application();
 
+console.log("Started server!");
+
 app.get("/", (c) => {
     return "Hello, world!";
 }).start({ port: 8080 });
