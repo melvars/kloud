e9aa61e7fb8c048afe96d000b7ba0cb7261791f1
LarsVomMars <lars@kroenner.eu>
2020-07-09 23:58:30 +0200

    Boilerplate, debugger, modules, ...

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { Application } from "https://deno.land/x/abc@v1/mod.ts";
+import { Application, Context } from "https://deno.land/x/abc@v1/mod.ts";
+import "https://deno.land/x/dotenv/load.ts";
 
+const port = parseInt(Deno.env.get("PORT") || "8080");
 const app = new Application();
 
-console.log("Started server!");
+app.static("/", "./src/public/"); // Manage static files
+app.file("/", "./src/public/index.html"); // Render index on /
 
-app.get("/", (c) => {
-    return "Hello, world!";
-}).start({ port: 8080 });
+app.start({ port });
+console.log(`Server listening on http://localhost:${port}`);
