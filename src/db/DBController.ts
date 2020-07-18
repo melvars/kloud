47f66e980761ad1218f51c7f51e51dd550bf0b35
Marvin Borner <develop@marvinborner.de>
2020-07-18 15:54:13 +0200

    Data, data, daaaaaaaaataaaaaaaaaaaa!

--- src/db/DBController.ts
+++ src/db/DBController.ts
@@ -2,19 +2,21 @@ import { Client } from "https://deno.land/x/mysql/mod.ts";
 import { readFileStr } from "https://deno.land/std/fs/mod.ts";
 
 export default class DBController {
+    private client?: Client;
+
     async init() {
-        const conn = await this.connect();
+        this.client = await this.connect();
         try {
             const sql = await readFileStr("./src/db/tables.sql");
             const queries = sql.split(";");
             queries.pop();
-            queries.forEach((query) => conn.execute(query));
+            queries.forEach(async (query) => await this.execute(query));
             console.log("Tables created");
         } catch (e) {
             console.error("Could not create tables");
             throw e;
         } finally {
-            conn.close();
+            this.client.close();
         }
     }
 
@@ -30,5 +32,27 @@ export default class DBController {
             console.error("Could not connect to database");
             throw e;
         }
+    }
+
+    async execute(query: string) {
+        if (this.client) {
+            try {
+                return await this.client.execute(query);
+            } catch (e) {
+                throw e;
+            }
+        } else throw Error("Database isn't initialized yet!");
+    }
+
+    async execute_multiple(queries: string[]) {
+        await this.client!.transaction(async (conn) => {
+            queries.forEach(async (query) => {
+                await conn.execute(query);
+            });
+        });
+    }
+
+    async close() {
+        await this.client!.close();
     }
 }
