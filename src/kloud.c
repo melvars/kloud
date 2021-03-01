37c19d5cd4ac80e56e629209c966231e1bd3abf2
Marvin Borner <develop@marvinborner.de>
2021-03-01 15:41:12 +0100

    Formatting and flags

--- src/kloud.c
+++ src/kloud.c
@@ -1,10 +1,9 @@
 #include <kore/kore.h>
+
 #include <kore/http.h>
 
-int		page(struct http_request *);
-
-int
-page(struct http_request *req)
+int page(struct http_request *);
+int page(struct http_request *req)
 {
 	http_response(req, 200, NULL, 0);
 	return (KORE_RESULT_OK);
